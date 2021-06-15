6cb1f786416074b175e5622affbeda5906dde83c
alimbanza <m.alimbanza@gmail.com>
2021-06-15 14:10:35 +0200

    authentification et gestion provinces

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\ProvinceController;
+
+Route::post('login', [ApiController::class, 'authenticate']);
+Route::post('register', [ApiController::class, 'register']);
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('logout', [ApiController::class, 'logout']);
+    Route::get('get_user', [ApiController::class, 'get_user']);
+    Route::get('products', [ProvinceController::class, 'index']);
+    Route::get('province', [ProvinceController::class, 'index']);
+    Route::post('province', [ProvinceController::class, 'store']);
+    // Route::post('create', [ProductController::class, 'store']);
+    // Route::put('update/{product}',  [ProductController::class, 'update']);
+    // Route::delete('delete/{product}',  [ProductController::class, 'destroy']);
+});
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +33,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
